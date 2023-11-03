731c7baae7e8de03956aeee0b27327e7cefa4f0c
Junichi Kashiyama <jun-k@users.noreply.github.com>
2023-11-03 07:46:37 +0000

    chap1-7

---
+++ JavaSilver_samplecode/chap1/7/com/se/Main.java
@@ -0,0 +1,10 @@
+package com.se;
+import com.se.ren.Foo;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("I am Main.");
+        Foo f = new Foo();
+        f.print();
+    }
+}
---
+++ JavaSilver_samplecode/chap1/7/com/se/ren/Foo.java
@@ -0,0 +1,8 @@
+package com.se.ren;
+
+public class Foo {
+    public void print() {
+        System.out.println("I am Foo.");
+        System.out.println("Foo#print()");
+    }
+}
