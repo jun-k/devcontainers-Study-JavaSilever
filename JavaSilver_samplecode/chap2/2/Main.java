ced260ce46354ce7b15dbf9eea9466d09af0f014
Junichi Kashiyama <jun-k@users.noreply.github.com>
2023-11-03 11:00:36 +0000

    chap2-2

---
+++ JavaSilver_samplecode/chap2/2/Main.java
@@ -0,0 +1,17 @@
+// package JavaSilver_samplecode.chap2.2;
+
+public class Main {
+    public static void main(String[] args) {
+        // float x1 = 3_.1415f;
+        // float x1 = 3._1415f;
+        // long x3 = 999_99_9999_L;
+        // int x4 = _52;
+        int x5 = 5_2;
+        // int x6 = 52_;
+        int x7 = 5_____2;
+        // int x8 = 0_x52;
+        // int x9 = 0x_52;
+        int x10 = 0x5_2;
+        int x11 = 0_52;
+    } 
+}
