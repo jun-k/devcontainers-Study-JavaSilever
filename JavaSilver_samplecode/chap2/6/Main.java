36232094ea98c5daef92ac93b8f4fed6cb1d4264
Junichi Kashiyama <jun-k@users.noreply.github.com>
2023-11-12 08:44:36 +0000

    chap2-6

---
+++ JavaSilver_samplecode/chap2/6/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        {
+            int x = 100;
+            x = 200;
+            System.out.println(x);
+        }
+        // System.out.println(x);  // コンパイルエラー
+    }
+}
